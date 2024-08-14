fa09be297a3ddad0baee6303f4b717051e568eae
Simone <151656886+simone013124@users.noreply.github.com>
2024-08-14 10:59:05 +0200

    responsive mobile

--- components/KontaktComponent.js
+++ components/KontaktComponent.js
@@ -41,7 +41,7 @@ export default class KontaktComponent extends KWM_Component {
            <div class="contactLink" role="group">
                  <button onclick="window.open('https://www.instagram.com/ferienhaus_puerstl/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA%3D%3D', '_blank')" class="btn btn-primary">Instagram</button>
                  <button onclick="window.open('https://www.facebook.com/profile.php?id=100075966014770', '_blank')" class="btn btn-primary">Facebook</button>
-                 <button onclick="window.open('https://www.google.at/maps/place/Ferienhaus+P%C3%BCrstl/@47.388498,13.5924077,18z/data=!4m6!3m5!1s0x47712ef87d250785:0xd079c4d86d0d0b4!8m2!3d47.3884345!4d13.5936093!16s%2Fg%2F11hzv5x18g?entry=tts', '_blank')" class="btn btn-primary">Google Maps</button>
+                 <button onclick="window.open('https://www.google.at/maps/place/Ferienhaus+P%C3%BCrstl/@47.388498,13.5924077,18z/data=!4m6!3m5!1s0x47712ef87d250785:0xd079c4d86d0d0b4!8m2!3d47.3884345!4d13.5936093!16s%2Fg%2F11hzv5x18g?entry=tts', '_blank')" class="btn btn-primary">Maps</button>
            </div>
            
        
